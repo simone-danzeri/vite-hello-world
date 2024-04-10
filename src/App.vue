3c14771792f13874a63a083b7d2c97196d48e7ab
simone-danzeri <s.danzeri@icloud.com>
2024-04-10 14:18:42 +0200

    feat: completo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script>
+  import AppTitle from './components/AppTitle.vue';
+  export default {
+    components: {
+      AppTitle
+    }
+  }
   // Qui il JS
 </script>
 
 <template>
   <!-- Qui l'HTML -->
+  <AppTitle></AppTitle>
 </template>
 
 <style>
-  /* Qui il CSS */
+/* Qui il CSS */
 </style>
